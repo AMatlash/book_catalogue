79f143c1ea087332b22e0623b071418592b50c52
A.Matlash <a.matlash@dev.crmm.ru>
2019-10-09 22:05:22 +0300

    Removing vue initial files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,17 +7,11 @@ export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
-        {
+        /* {
             path: '/',
             name: 'home',
             component: Home
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // component: () => import(/* webpackChunkName: "about" */ '@views/About.vue')
-            component: () => import(/* webpackPrefetch: true */ '@views/About.vue')
-        },
+        }, */
         {
             path: '/ui-kit-pre',
             name: 'ui-kit-pre',
